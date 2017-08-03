b25fd973bd8fe205b6f4847a3b45189106d481dc
teodor.miu <miu.teo@gmail.com>
2017-08-03 15:50:36 +0300

    .

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
+  @Input() name : string;
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }

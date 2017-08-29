24b5ca8b3d57a87cfbb9dfc81da8aacf371e1dd5
teodor.miu <miu.teo@gmail.com>
2017-08-29 17:51:01 +0300

    formInAngular2

--- formsInAngular2/src/app/app.module.ts
+++ formsInAngular2/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Demo_form_sku} from "./demo_form_sku";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Demo_form_sku
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ formsInAngular2/src/app/demo_form_sku.ts
@@ -0,0 +1,28 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'demo-form-sku',
+  template: `<div class="ui raised segment">
+    <h2 class="ui header">Demo form:SKU</h2>
+    <form class="ui form"
+          #f="ngForm" 
+          (ngSubmit)="onSubmit(f.value)"
+    >
+      <div class="field">
+        <label for="skuInput">SKU</label>
+        <input type="text"
+               id="skuInput"
+               placeholder="SKU"
+               name="SKU"
+        >
+      </div>
+      
+      <button type="submit" class="ui button">Submit</button>
+    </form>
+  </div>`
+})
+export class Demo_form_sku{
+  onSubmit(object){
+    console.log(`object value=`,object.value)
+  }
+}

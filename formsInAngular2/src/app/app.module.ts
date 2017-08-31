634222ec10c3b277d57851d7626064326a81739e
teodor.miu <miu.teo@gmail.com>
2017-08-31 17:15:08 +0300

    basic form with builder & validation part2

--- formsInAngular2/src/app/app.module.ts
+++ formsInAngular2/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {Demo_form_sku} from "./demo_form_sku";
 import {DemoFormSkuWithBuilder} from "./demo_form_sku_with_builder";
 import {DemoFormWithValidation} from './demo_form_with_validation';
+import {DemoFormWithValidationShorthand} from "./demo_form_with_validations_shorthand";
 
 @NgModule({
   declarations: [
     AppComponent,
     Demo_form_sku,
     DemoFormSkuWithBuilder,
-    DemoFormWithValidation
+    DemoFormWithValidation,
+    DemoFormWithValidationShorthand
   ],
   imports: [
     BrowserModule,
---
+++ formsInAngular2/src/app/demo_form_with_validations_shorthand.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector : 'demo-form-with-validation-shorthand',
+  template : `<div class="ui raised segment">
+    <h2 class="ui header">Demo form with validation shorhand</h2>
+    <form class="ui form" [formGroup]="myForm"
+        (ngSubmit)="onSubmit(myForm.value)">
+      <div class="field" [class.error]="!myForm.controls['sku'].valid && myForm.touched">
+        <label for="skuInput">SKU</label>
+        <input type="text"
+               id="skuInput"
+               placeholder="SKU"
+               name="SKU"
+               [formControl]="myForm.controls['sku']"
+        >
+        <div *ngIf="!myForm.controls['sku'].valid" class="ui error message">SKU is invalid</div>
+        <div *ngIf="myForm.controls['sku'].hasError('required') && myForm.controls['sku'].touched">SKU is required</div>
+        <div *ngIf="myForm.controls['sku'].hasError('min') && myForm.controls['sku'].touched">Min value is 10</div>
+        <div *ngIf="!myForm.valid" class="ui error message">Form is invalid</div>
+      </div>
+      <button type="submit" class="ui button">Submit</button>
+    </form>
+  </div>`
+})
+export class DemoFormWithValidationShorthand{
+  myForm : FormGroup;
+
+  constructor(fb:FormBuilder){
+    this.myForm = fb.group({
+      'sku':['',[Validators.required,Validators.min(10)]]
+    });
+  }
+  onSubmit(object:any){
+    console.log(`object value=`,object);
+  }
+
+}
